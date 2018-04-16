9b51e0654f28eb915b8a68dc3a4ff98ad7997c8f
Sara Chipps <sarajchipps@gmail.com>
2018-04-16 17:28:45 -0400

    initial commit before adding tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule
