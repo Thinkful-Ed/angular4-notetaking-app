11fccb792c091961312b1d98197a760b62b7c39d
Sara Chipps <sarajchipps@gmail.com>
2018-04-29 11:51:12 -0400

    added observable

--- src/app/note.service.ts
+++ src/app/note.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Note } from './note';
 import { NOTES } from './mock-data';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class NoteService {
 
   constructor() { }
-  getNotes(): Note[] {
-    return NOTES;
+  getNotes(): Observable<Note[]> {
+    return of(NOTES);
   }
 }
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -14,7 +14,8 @@ export class NotesComponent implements OnInit {
   constructor(private noteService: NoteService) { }
 
   getNotes(): void {
-    this.notes = this.noteService.getNotes();
+    this.noteService.getNotes()
+        .subscribe(notes => this.notes = notes);
   }
   ngOnInit() {
       this.getNotes();
