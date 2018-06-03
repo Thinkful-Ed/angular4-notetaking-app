058ddad2d7c98e1c0f82726cfd80f6024d0f4410
Sara Chipps <sarajchipps@gmail.com>
2018-06-03 17:55:32 -0400

    added tests

--- src/app/notes/notes.component.spec.ts
+++ src/app/notes/notes.component.spec.ts
@@ -1,25 +1,34 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { NotesComponent } from "./notes.component";
 
-import { NotesComponent } from './notes.component';
-
-describe('NotesComponent', () => {
+describe("NotesComponent", () => {
   let component: NotesComponent;
   let fixture: ComponentFixture<NotesComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NotesComponent ]
+  let app;
+  let compiled;
+  
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        declarations: [NotesComponent],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NotesComponent);
     component = fixture.componentInstance;
+    app = fixture.debugElement.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+  it("should have a list for notes" + location, () => {
+    expect(compiled.querySelector("ul").nodeName).toEqual("UL");
   });
 });
