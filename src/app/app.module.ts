1fbd694d29a2758e166e482afc0c8fc5f86a0649
Sara Chipps <sarajchipps@gmail.com>
2018-06-03 22:54:14 -0400

    Added Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 
 import { NotesService } from './notes.service';
 
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
 import { CreateNoteComponent } from './create-note/create-note.component';
+import { FoldersComponent } from './folders/folders.component';
 
+
+const appRoutes: Routes = [
+  { path: 'notes', component: CreateNoteComponent },
+  { path: 'folders', component: FoldersComponent },
+  { path: '**',
+    redirectTo: '/notes',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesComponent,
-    CreateNoteComponent
+    CreateNoteComponent,
+    FoldersComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+     appRoutes,
+     { enableTracing: true } // <-- debugging purposes only
+    ),
   ],
   providers: [ NotesService ],
   bootstrap: [AppComponent]
---
+++ src/app/notes.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { NOTES } from "./mock-data";
+import { Note } from "./models";
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+@Injectable()
+export class NotesService {
+  getNotes(): Observable<Note[]> {
+    return of(NOTES);
+  }
+  constructor() {}
+}
