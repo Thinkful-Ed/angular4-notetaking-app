185e44c7ff333928b747f780b72f70b767636a97
Sara Chipps <sarajchipps@gmail.com>
2018-06-04 15:46:38 -0400

    added base service

---
+++ src/app/base.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
+@Injectable()
+export class BaseService {
+  baseUrl = "https://notefulapp.herokuapp.com/api";
+
+}
